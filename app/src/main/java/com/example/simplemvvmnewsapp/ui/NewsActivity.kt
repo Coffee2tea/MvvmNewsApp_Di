a7e0aade4ba0df7838e9f4900fa982a7d4b5a94e
shener1974@gmail.com <mezi1976>
2021-09-09 12:01:31 -0400

    Top menu added. Drawer menu added. Font size on Appbar changed.

--- app/src/main/java/com/example/simplemvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/simplemvvmnewsapp/ui/NewsActivity.kt
@@ -2,9 +2,17 @@ package com.example.simplemvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.example.simplemvvmnewsapp.R
+import com.example.simplemvvmnewsapp.ui.fragments.BreakingNewsFragment
+import com.example.simplemvvmnewsapp.ui.fragments.SavedNewsFragment
+import com.example.simplemvvmnewsapp.ui.fragments.SearchNewsFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_news.*
 
@@ -13,12 +21,40 @@ import kotlinx.android.synthetic.main.activity_news.*
 @AndroidEntryPoint
 class NewsActivity : AppCompatActivity() {
 
+        lateinit var toggle_button: ActionBarDrawerToggle
+
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_news)
 
+        toggle_button = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle_button)
+        toggle_button.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setupWithNavController(newsNavHostFragment.findNavController())
+
+
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle_button.onOptionsItemSelected(item)){
+            return true
+        }
+
+        val navController = findNavController(R.id.newsNavHostFragment)
+        return item.onNavDestinationSelected(navController)
+
     }
 
 
---
+++ app/src/main/java/com/example/simplemvvmnewsapp/ui/fragments/SettingFragment.kt
@@ -0,0 +1,23 @@
+package com.example.simplemvvmnewsapp.ui.fragments
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
+import com.example.simplemvvmnewsapp.R
+import com.example.simplemvvmnewsapp.main.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_article.*
+
+@AndroidEntryPoint
+class SettingFragment: Fragment(R.layout.fragment_setting) {
+
+
+}
+
